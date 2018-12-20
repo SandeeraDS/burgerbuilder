1a102bcf81671fda30ddcdf3ab70fe92ed9572e0
Dilusha Sandeera Jayampathi <32511955+SandeeraDS@users.noreply.github.com>
2018-12-20 16:26:06 +0530

    140

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import classes from './Toolbar.module.css'
-import Logo from '../../Logo/Logo'
-import NavigationItems from '../NavigationItems/NavigationItems'
+import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
+//import drawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
-const toolbar =()=>(
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <div className={classes.Logo}> <Logo></Logo></div>
-      
-        <nav className={classes.DesktopOnly}><NavigationItems/></nav>
-    </header>
-)
+const toolbar = props => (
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
 
 export default toolbar;
